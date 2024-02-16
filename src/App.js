671a7bb7c9303c4aefce5edc746bc43aea10258d
Michael Rutherford <mtruth65@gmail.com>
2024-02-16 15:31:41 +1000

    Before The Start of Star Wars programming

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './MyItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Item name="Kwila" />
+        <Item name="Acacia" />
+        <Item name="Celia" />
+        <Item name="Michael" />
         <a
           className="App-link"
           href="https://reactjs.org"
